6bee4a524ef075b1b47fc3a32de736a32245a3c7
AbdelrahmanRagab38 <abdelrahmanragab38@gmail.com>
2024-05-19 12:33:39 +0300

    fix : fix the prompt of the open ai model

--- llm_models/model_inference.py
+++ llm_models/model_inference.py
@@ -19,20 +19,24 @@ def format_data(df: DataFrame, order_details_json: dict) -> Tuple[str, str]:
     :param (dict) order_details_json: The order details as a dictionary
     :return: (Tuple[str, str]) The formatted dataset and order query
     """
+    df.columns = ['OfferName', 'MobileData', 'Order_status', 'TicketTitle', "Number of Records"]
 
     def prefix_entry(entry, col_name):
         """
         Prefix the entry with the column name for formatting.
         """
         if entry == 0:
-            return 'Not Complained'
+            return 'Order_status:Not Complained'
         elif entry == 1:
-            return 'Order Complained'
+            return 'Order_status:Complained'
         return f"{col_name}:{entry}"
 
     formatted_df = df.apply(lambda col: col.apply(prefix_entry, col_name=col.name))
+    #remove column names
+    #formatted_df.columns = ['' for col in formatted_df.columns]
     dataset = formatted_df.to_string(index=False)
-    order_query = f"OfferName: {order_details_json['OfferName']} ,MobileData: {order_details_json['MobileData']}GB"
+    order_query = (f"- OfferName: {order_details_json['OfferName']} \n"
+                   f"\t- MobileData: {order_details_json['MobileData']}GB")
     return dataset, order_query
 
 
@@ -45,26 +49,30 @@ def send_prompt(client: OPENAI, dataset: str, order_query: str) -> dict:
     :param (str) order_query: The formatted order query
     :return: (dict) The response from the OpenAI model
     """
-    template_text = f"""
-    Analyze the provided order data for complaints and ticket types.
-    Given the dataset taking in consideration that Number of Records represents the number of occurence of unique orders with the compination of OfferName and MobileData fields in our dataset:
+    template_text = f""" System: You are a data analysis tool specialized in identifying potential complaints from 
+    order data. Use the provided dataset to analyze and predict the likelihood of complaints for specific orders 
+    based on historical data.
 
-    {dataset}
+      Dataset:
+      {dataset}
 
-    For the orders details data frame: '{order_query}', predict based on the compination of OfferName and MobileData fields represented in the previous data frame:
-    1. The likelihood of a complaint predict the value of the Complained filed for the given oredr (0 or 1)
-    2. The probability of complaint occurrence based on the records count as a float number from 0 to 1 rounded to two decimal points
-    3. The likely ticket type.
-    4- Generate a text to recommend that the solution caused by Catalogue mismatch detected, a new modify order has been created by Self Healing engine
+      Order Details:
+        {order_query}
+        
+      Task:
+      1. Predict whether the order with the details provided will result in a complaint (0 for no complaint, 1 for complaint).
+      2. Estimate the probability of a complaint occurrence for this order, expressed as a decimal from 0 to 1 rounded to two decimal points.
+      3. Predict the likely ticket title if a complaint occurs.
+      4. Suggest a solution if a complaint is detected, considering past data where a similar issue was addressed by a self-healing engine modifying the order.
 
-    Format the response as a json object with the fields:
-    - Potential_Complaint
-    - Probability_of_Complaint
-    - TicketTitle
-    - Recommended_solution
-    """
+      Format the response as a JSON object with the fields:
+      - Potential_Complaint
+      - Probability_of_Complaint
+      - TicketTitle
+      - Recommended_solution
+      """
     response = client.chat.completions.create(
-        model="gpt-3.5-turbo-0125",
+        model="gpt-4-turbo",
         response_format={"type": "json_object"},
         messages=[
             {"role": "system", "content": "You are a helpful assistant."},
